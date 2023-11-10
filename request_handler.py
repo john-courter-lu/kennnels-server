98f4497ac675a7d191e7fb840e57b80436b9506c
John Lu <johnlu2006@gmail.com>
2023-11-10 14:42:15 -0600

    Split and Exceptions and Tuples

--- request_handler.py
+++ request_handler.py
@@ -43,6 +43,30 @@ class HandleRequests(BaseHTTPRequestHandler):
         # Send a JSON formatted string as a response
         self.wfile.write(json.dumps(response).encode())
 
+    # This method takes a single input - the path of the request
+    # and returns a tuple.
+    def parse_url(self, path):
+        """Split and Exceptions and Tuples"""
+        # Just like splitting a string in JavaScript. If the
+        # path is "/animals/1", the resulting list will
+        # have "" at index 0, "animals" at index 1, and "1"
+        # at index 2.
+        path_params = path.split("/")
+        resource = path_params[1]
+        id = None
+
+        # Try to get the item at index 2
+        try:
+            # Convert the string "1" to the integer 1
+            # This is the new parseInt()
+            id = int(path_params[2])
+        except IndexError:
+            pass  # No route parameter exists: /animals
+        except ValueError:
+            pass  # Request had trailing slash: /animals/
+
+        return (resource, id)  # This is a tuple
+
     # Here's a method on the class that overrides the parent's method.
     # It handles any POST request.
     def do_POST(self):
